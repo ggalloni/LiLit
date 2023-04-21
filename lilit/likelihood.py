acb815929f536dbb9e009879ee6d2c0a1a4ac25e
piccij <giulia.piccirilli@roma2.infn.it>
2023-04-21 17:17:10 +0200

    updated b(z) for Euclid

--- lilit/likelihood.py
+++ lilit/likelihood.py
@@ -823,13 +823,11 @@ class LiLit(Likelihood):
 
         z_med = [(z_bin_m[i + 1] + z_bin_m[i]) / 2 for i in range(len(z_bin_m) - 1)]
 
-        bz_step = np.zeros(len(self.zz))
-        for i, z in enumerate(self.zz):
+        bz_step = np.zeros(len(zz))
+        for i, z in enumerate(zz):
             for j in range(len(z_med)):
-                if z >= z_med[j] and z <= z_med[j + 1]:
+                if z >= z_bin_m[j] and z <= z_bin_m[j + 1]:
                     bz_step[i] = np.sqrt(1 + z_med[j])
-                else:
-                    bz_step[i] = 0
 
         self.bz_step = [bz_step for i in range(len(z_bin_m) - 1)]
 
