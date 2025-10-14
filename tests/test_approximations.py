3524e8437a27cc2122c1c0afdde411874ff01900
Giacomo Galloni <giacomo.galloni@unife.it>
2025-10-14 15:27:27 +0200

    relax for machine dependence

--- tests/test_approximations.py
+++ tests/test_approximations.py
@@ -10,7 +10,7 @@ def test_exact(info_dict, exact_likelihood, ref_like_values):
     model = get_model(info_dict)
     loglike = model.loglikes()[0][0]
     print(loglike)
-    np.testing.assert_almost_equal(loglike, ref_like_values["exact"], decimal=10)
+    np.testing.assert_almost_equal(loglike, ref_like_values["exact"], decimal=5)
 
 def test_gaussian(info_dict, gauss_likelihood, ref_like_values):
     """Test gaussian likelihood in isolation to check for initialization issues."""
@@ -18,7 +18,7 @@ def test_gaussian(info_dict, gauss_likelihood, ref_like_values):
     model = get_model(info_dict)
     loglike = model.loglikes()[0][0]
     print(loglike)
-    np.testing.assert_almost_equal(loglike, ref_like_values["gauss"], decimal=10)
+    np.testing.assert_almost_equal(loglike, ref_like_values["gauss"], decimal=5)
 
 def test_correlated(info_dict, correlated_likelihood, ref_like_values):
     """Test correlated likelihood in isolation to check for initialization issues."""
@@ -26,7 +26,7 @@ def test_correlated(info_dict, correlated_likelihood, ref_like_values):
     model = get_model(info_dict)
     loglike = model.loglikes()[0][0]
     print(loglike)
-    np.testing.assert_almost_equal(loglike, ref_like_values["correlated"], decimal=10)
+    np.testing.assert_almost_equal(loglike, ref_like_values["correlated"], decimal=5)
 
 def test_hl(info_dict, hl_likelihood, ref_like_values):
     """Test Hamimeche-Lewis likelihood in isolation to check for initialization issues."""
@@ -34,7 +34,7 @@ def test_hl(info_dict, hl_likelihood, ref_like_values):
     model = get_model(info_dict)
     loglike = model.loglikes()[0][0]
     print(loglike)
-    np.testing.assert_almost_equal(loglike, ref_like_values["hl"], decimal=10)
+    np.testing.assert_almost_equal(loglike, ref_like_values["hl"], decimal=5)
 
 def test_lollipop(info_dict, lollipop_likelihood, ref_like_values):
     """Test lollipop likelihood in isolation to check for initialization issues."""
@@ -42,4 +42,4 @@ def test_lollipop(info_dict, lollipop_likelihood, ref_like_values):
     model = get_model(info_dict)
     loglike = model.loglikes()[0][0]
     print(loglike)
-    np.testing.assert_almost_equal(loglike, ref_like_values["lollipop"], decimal=10)
+    np.testing.assert_almost_equal(loglike, ref_like_values["lollipop"], decimal=5)
